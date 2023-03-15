a60c4e010de033f4fb94d47915129a5089ac5101
Bryan-Lor <103898129+Bryan-Lor@users.noreply.github.com>
2023-03-15 00:54:47 -0400

    added theme toggle UI

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { useSettingsContext } from "~/data/ThemeContext";
+import { FaToggleOff, FaToggleOn } from "react-icons/fa";
 
 const Nav: React.FC = () => {
   const { darkTheme, setDarkTheme } = useSettingsContext();
@@ -9,10 +10,10 @@ const Nav: React.FC = () => {
       <nav className="text-difference  absolute top-0 right-0 z-[1] w-full px-[1.25rem] py-[0.625rem] text-right">
         <button
           id="toggleDark"
-          className="text-difference mr-[1rem]"
+          className="text-difference mr-[1rem] h-[2rem] text-center align-middle"
           onClick={() => setDarkTheme(!darkTheme)}
         >
-          Toggle
+          {darkTheme ? <FaToggleOn /> : <FaToggleOff />}
         </button>
         <ul className="inline-flex gap-[1rem] text-right">
           <li>
