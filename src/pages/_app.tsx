e47679f82604351c4d5e235ddd3f23d48b9bd130
Bryan-Lor <103898129+Bryan-Lor@users.noreply.github.com>
2023-03-25 02:49:02 -0400

    Fixed fetch error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,9 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 import React, { useEffect, useState } from "react";
 import { ThemeContext } from "~/data/ThemeContext";
-import { EventsContext, EventType } from "~/data/EventsContext";
+import { EventsContext, EventType, rssUrl } from "~/data/EventsContext";
 import "~/styles/globals.css";
+// import Parser from "rss-parser";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   const [darkTheme, setDarkTheme] = useState<boolean>(true);
@@ -16,6 +17,32 @@ const MyApp: AppType = ({ Component, pageProps }) => {
     }
     fetchEvents();
   }, []);
+
+  // async function fetchEvents() {
+  // const feed = await new Parser().parseURL(rssUrl);
+
+  //   if (feed.items.length > 0) {
+  //     const mappedEvents: EventType[] = feed.items.map(
+  //       (item): EventType => ({
+  //         author: item.author,
+  //         categories: item.categories,
+  //         content: item.content,
+  //         contentSnippet: item.contentSnippet,
+  //         creator: item.creator,
+  //         enclosure: item.enclosure,
+  //         guid: item.guid,
+  //         isoDate: item.isoDate,
+  //         link: item.link,
+  //         pubDate: item.pubDate,
+  //         title: item.title,
+  //       })
+  //     );
+  //     setEvents(mappedEvents);
+  //   }
+  // }
+  // void fetchEvents();
+
+  // console.log(events);
 
   // useEffect(() => {
   //   async function fetchEvents() {
