29465ff8ead6ff8d19deafe5d3430c83ba33334b
Bryan-Lor <103898129+Bryan-Lor@users.noreply.github.com>
2023-03-25 02:53:45 -0400

    Added Type back into Events in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import { ThemeContext } from "~/data/ThemeContext";
 import { EventsContext, EventType, rssUrl } from "~/data/EventsContext";
 import "~/styles/globals.css";
-// import Parser from "rss-parser";
+import Parser from "rss-parser";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   const [darkTheme, setDarkTheme] = useState<boolean>(true);
@@ -11,38 +11,33 @@ const MyApp: AppType = ({ Component, pageProps }) => {
 
   useEffect(() => {
     async function fetchEvents() {
-      const res = await fetch("/api/events");
-      const data: EventType[] = await res.json();
-      setEvents(data);
+      try {
+        const parser = new Parser<EventType[]>();
+        const feed = await parser.parseURL(rssUrl);
+        if (feed.items.length > 0) {
+          const mappedEvents: EventType[] = feed.items.map(
+            (item): EventType => ({
+              author: item.author,
+              categories: item.categories,
+              content: item.content,
+              contentSnippet: item.contentSnippet,
+              creator: item.creator,
+              enclosure: item.enclosure,
+              guid: item.guid,
+              isoDate: item.isoDate,
+              link: item.link,
+              pubDate: item.pubDate,
+              title: item.title,
+            })
+          );
+          setEvents(mappedEvents);
+        }
+      } catch (error) {
+        console.error("Error fetching events:", error);
+      }
     }
-    fetchEvents();
+    void fetchEvents();
   }, []);
-
-  // async function fetchEvents() {
-  // const feed = await new Parser().parseURL(rssUrl);
-
-  //   if (feed.items.length > 0) {
-  //     const mappedEvents: EventType[] = feed.items.map(
-  //       (item): EventType => ({
-  //         author: item.author,
-  //         categories: item.categories,
-  //         content: item.content,
-  //         contentSnippet: item.contentSnippet,
-  //         creator: item.creator,
-  //         enclosure: item.enclosure,
-  //         guid: item.guid,
-  //         isoDate: item.isoDate,
-  //         link: item.link,
-  //         pubDate: item.pubDate,
-  //         title: item.title,
-  //       })
-  //     );
-  //     setEvents(mappedEvents);
-  //   }
-  // }
-  // void fetchEvents();
-
-  // console.log(events);
 
   // useEffect(() => {
   //   async function fetchEvents() {
