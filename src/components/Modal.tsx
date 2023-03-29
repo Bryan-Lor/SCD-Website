102d77043cc286738763fa0514a4953ebaaced67
Bryan-Lor <103898129+Bryan-Lor@users.noreply.github.com>
2023-03-28 23:37:55 -0400

    iframe Size Test

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -7,20 +7,26 @@ const Modal: React.FC<{ title: string; desc: string; showMenu: Function }> = ({
 }) => {
   return (
     <div className="relative flex items-center justify-center">
-      <div
+      <button
+        onClick={() => {
+          showMenu;
+          console.log("clicked");
+        }}
+        aria-label="close"
         id="menu"
         className="sticky-0 fixed top-0 h-full w-full bg-gray-900 bg-opacity-80"
       >
-        <div className="flex  items-center justify-center py-48 px-4 2xl:container md:px-28 2xl:mx-auto">
-          <div className="relative flex w-96 flex-col items-center justify-center bg-gray-800 py-16 px-4 md:w-auto md:px-24 xl:py-24 xl:px-36">
-            <div role="banner w-full h-full bg-red-500">
-              {/* <iframe
+        <div className="flex items-center justify-center py-48 px-4 2xl:container md:px-28 2xl:mx-auto">
+          {/* px-4 md:w-auto md:px-24 xl:py-24 xl:px-36 w-96 goes to div below */}
+          <div className="container relative z-50 flex flex-col items-center justify-center bg-gray-800 py-16">
+            <div role="banner" className="h-full w-full bg-black">
+              <iframe
                 width="100%"
                 height="100%"
                 allowFullScreen
                 // className="h-full w-full bg-green-500"
                 src="https://www.youtube.com/embed/tgbNymZ7vqY"
-              ></iframe> */}
+              ></iframe>
             </div>
             <div className="mt-12">
               <h2
@@ -47,16 +53,16 @@ const Modal: React.FC<{ title: string; desc: string; showMenu: Function }> = ({
                 {desc}
               </p>
             </div> */}
-            <button
+            {/* <button
               onClick={showMenu}
               className="text-difference absolute top-8 right-8 focus:outline-none focus:ring-2 focus:ring-gray-800 focus:ring-offset-2"
               aria-label="close"
             >
               x
-            </button>
+            </button> */}
           </div>
         </div>
-      </div>
+      </button>
     </div>
   );
 };
