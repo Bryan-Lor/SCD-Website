bea0f08f68443167b62688897014a1f154b76713
Bryan-Lor <103898129+Bryan-Lor@users.noreply.github.com>
2023-03-29 23:47:58 -0400

    Test iFrame Size

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -16,16 +16,28 @@ const Modal: React.FC<{ title: string; desc: string; showMenu: Function }> = ({
         id="menu"
         className="sticky-0 fixed top-0 h-full w-full bg-gray-900 bg-opacity-80"
       >
-        <div className="flex items-center justify-center py-48 px-4 2xl:container md:px-28 2xl:mx-auto">
+        <div className="flex h-[50vh] items-center justify-center py-48 px-4 2xl:container md:px-28 2xl:mx-auto">
           {/* px-4 md:w-auto md:px-24 xl:py-24 xl:px-36 w-96 goes to div below */}
           <div className="container relative z-50 flex flex-col items-center justify-center bg-gray-800 py-16">
-            <div role="banner" className="h-full w-full bg-black">
-              <iframe
+            {/* <div role="banner" className="h-[42rem] w-full bg-black"> */}
+            <div role="banner" className="h-[42vh] w-full bg-black">
+              {/* <iframe
                 width="100%"
                 height="100%"
                 allowFullScreen
                 // className="h-full w-full bg-green-500"
                 src="https://www.youtube.com/embed/tgbNymZ7vqY"
+              ></iframe> */}
+              <iframe
+                // width="560"
+                // height="315"
+                width="100%"
+                height="100%"
+                src="https://www.youtube.com/embed/nUOMTViKsko"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allowfullscreen
               ></iframe>
             </div>
             <div className="mt-12">
